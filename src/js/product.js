d79aa55e4df0815357567492c8b8fcc9e15a9a0c
Antonio Melendez <mel23001@byui.edu>
2024-06-29 14:14:39 -0600

    fixing netlify

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,7 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-
+//there was a mistake on the wording here on my part
 loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
